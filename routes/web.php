7eb97c8a340156a59e6b7397b9aa6af9b94431ae
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-20 09:51:38 +0600

    list and show products with dummy data

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
 Route::get('/about', [\App\Http\Controllers\HomeController::class, 'about'])->name('home.about');
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
